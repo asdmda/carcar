cf09d8f1b2c5878c76407a934492e6d667a97479
asdmda <70678901+asdmda@users.noreply.github.com>
2024-06-10 16:01:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleBMWModels() {
+    var bmwModels = document.getElementById('bmw-models');
+    if (bmwModels.style.display === 'none') {
+        bmwModels.style.display = 'block';
+    } else {
+        bmwModels.style.display = 'none';
+    }
+}
+
+function toggleHyundaiModels() {
+    var hyundaiModels = document.getElementById('hyundai-models');
+    if (hyundaiModels.style.display === 'none') {
+        hyundaiModels.style.display = 'block';
+    } else {
+        hyundaiModels.style.display = 'none';
+    }
+}
